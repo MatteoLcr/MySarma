1c1c3de003d072a19c80a11965df02c5cdb21fa5
Matteo Lucarelli <matteo.lucarelli93@gmail.com>
2025-03-07 16:53:23 +0100

    prova2

--- src/components/box3/box3_2.vue
+++ src/components/box3/box3_2.vue
@@ -2,8 +2,8 @@
 import { ArrowNarrowRightIcon } from '@heroicons/vue/solid';
 import Guru_img from '../../assets/box3/Guru_img.png';
 import Pianoforte from '../../assets/box3/Pianoforte.png';
-import Servizi_img from '../../assets/box3/Servizi_img.png';
-import Piattaforma_img from '../../assets/box3/Piattaforma_img.png';
+import Servizi from '../../assets/box3/Servizi.png';
+import Piattaforma from '../../assets/box3/Piattaforma.png';
 </script>
 
 <template>
@@ -33,7 +33,7 @@ import Piattaforma_img from '../../assets/box3/Piattaforma_img.png';
 
         <div class="flex flex-col rounded-4xl mt-10" style="background-color: #426BD1; width: 95%;">
             <h3 class="text-white size48 text-start ms-10 mt-8">Soluzioni welfare esclusive</h3>
-            <img :src="Piattaforma_img" alt="Welfare Guru" style="scale: 1;" />
+            <img :src="Piattaforma" alt="Welfare Guru" style="scale: 1;" />
             <button class="flex justify-center items-center size16 text-white mb-10 mt-8">
                 <ArrowNarrowRightIcon class="w-7 h-5 me-5" />
                 SCOPRI DI PIU'
@@ -42,7 +42,7 @@ import Piattaforma_img from '../../assets/box3/Piattaforma_img.png';
 
         <div class="flex flex-col rounded-4xl mt-10" style="background-color: #299882; width: 95%;">
             <h3 class="text-white size48 text-start ms-10 mt-8">Piattaforma web dedicata</h3>
-            <img :src="Servizi_img" alt="Welfare Guru" style="scale: 1;" />
+            <img :src="Servizi" alt="Welfare Guru" style="scale: 1;" />
             <button class="flex justify-center items-center size16 text-white mb-10">
                 <ArrowNarrowRightIcon class="w-7 h-5 me-5" />
                 SCOPRI DI PIU'
